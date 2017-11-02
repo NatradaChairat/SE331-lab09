69bac7f2d152664371dba9b9376e935e7a8c3b8c
Natrada Chairat <natrada_chairat@cmu.ac.th>
2017-11-03 00:47:21 +0700

    2.5

--- web/app/students/course-list/course-list.component.ts
+++ web/app/students/course-list/course-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'course-list',
@@ -9,6 +9,7 @@ export class CourseListComponent implements OnInit{
   constructor(){
 
   }
+  @Input() count:number;
   ngOnInit(){
 
   }
